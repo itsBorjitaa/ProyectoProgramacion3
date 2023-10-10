91af42ffced6da2fa91fb85a4fbcbbd2a73b315a
Borja Rabade <borja.rabade@opendeusto.es>
2023-10-10 17:23:46 +0200

    Añadido:

--- ProyectoProgramacion3/src/GestionFacturas/VentanaPrincipal.java
+++ ProyectoProgramacion3/src/GestionFacturas/VentanaPrincipal.java
@@ -2,15 +2,43 @@ package GestionFacturas;
 
 import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTable;
 
 public class VentanaPrincipal extends JFrame{
-	private JTable tabla;
-	private List<Factura> facturas;
+	
+	private JPanel panelBotones;
+	private JPanel panelCalendario;
+	private JButton botonCategorias;
+	private JButton botonGastos;
+	private JButton botonCerrarSesion;
+	private JButton botonMeses;
+	private JButton botonAños;
 	
 	public VentanaPrincipal() {
-		setBounds(500,200,600,500);
+		
+		/**
+		 * Paneles de la ventana principal
+		 */
+		panelBotones = new JPanel();
+		panelCalendario = new JPanel();
+		
+		/**
+		 * Botones del panel de botones
+		 */
+		botonCategorias = new JButton();
+		botonGastos = new JButton();
+		botonCerrarSesion = new JButton();
+		
+		/**
+		 * Botones del panel del calendario
+		 */
+		botonMeses = new JButton();
+		botonAños = new JButton();
+		
+		setBounds(415,200,700,500);
 		setTitle("Aplicación de Gestión de Facturas");
 		setVisible(true);
 	}
