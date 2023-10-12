2760cd95186369f1b31af81e1c5e9675c5e28e17
Adrian Roman <adrian.roman@opendeusto.es>
2023-10-12 17:07:06 +0200

    Creacion clase Usuario

--- ProyectoProgramacion3/src/gestionFacturas/Usuario.java
+++ ProyectoProgramacion3/src/gestionFacturas/Usuario.java
@@ -1,5 +1,37 @@
 package gestionFacturas;
 
 public class Usuario {
+	private String usuario, contrasenya;
 
+	public Usuario(String usuario, String contrasenya) {
+		super();
+		this.usuario = usuario;
+		this.contrasenya = contrasenya;
+	}
+	
+	public Usuario() {
+	}
+	
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getContrasenya() {
+		return contrasenya;
+	}
+
+	public void setContrasenya(String contrasenya) {
+		this.contrasenya = contrasenya;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [usuario=" + usuario + ", contrasenya=" + contrasenya + "]";
+	}
+	
+	
 }
