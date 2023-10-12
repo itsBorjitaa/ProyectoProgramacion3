af9d1b28aaea74e3770b783d4b3c86d7ee17ed31
Sergio Martínez García <sergio.m@opendeusto.es>
2023-10-12 18:28:01 +0200

    Creación de la ventana de gastos

---
+++ ProyectoProgramacion3/src/gestionFacturas/VentanaGastos.java
@@ -0,0 +1,60 @@
+package gestionFacturas;
+
+import javax.swing.table.DefaultTableModel;
+import javax.swing.*;
+import java.awt.*;
+
+public class VentanaGastos extends JFrame{
+	
+	private JPanel panelTabla,panelBotones;
+	private DefaultTableModel tablaGastos;
+	private JRadioButton botonMes,botonTrimestre,botonAnyo;
+	private ButtonGroup grupoBotones;
+	
+	public VentanaGastos() {
+		/**
+		 * Paneles de la ventana gastos
+		 */
+		panelBotones=new JPanel(new GridLayout(4,1));
+		panelTabla=new JPanel();
+		
+		/**
+		 * Elementos del panel de botones
+		 */
+		botonMes=new JRadioButton("Mes");
+		botonTrimestre=new JRadioButton("Trimestre"); 
+		botonAnyo=new JRadioButton("Año");
+		grupoBotones=new ButtonGroup();
+		
+		/**
+		 * Configuramos el funcionamiento d elos botones
+		 */
+		grupoBotones.add(botonMes); grupoBotones.add(botonTrimestre); grupoBotones.add(botonAnyo);
+		botonMes.setSelected(true); //Hacemos que el boton del mes este seleccionado desde el principio
+		
+		/**
+		 * Añadimos los elementos al panel de botones
+		 */
+		JLabel textoBotones=new JLabel("Elige el intervalo que quieres ver:");
+		panelBotones.add(textoBotones);
+		panelBotones.add(botonMes);
+		panelBotones.add(botonTrimestre);
+		panelBotones.add(botonAnyo); 
+		panelBotones.setBorder(BorderFactory.createBevelBorder(1));
+		
+		/**
+		 * Añadimos la tabla al 
+		 */
+		
+		/**
+		 * Añadimos los paneles
+		 */
+		add(panelTabla,BorderLayout.NORTH);//Añadimos los paneles
+		add(panelBotones,BorderLayout.SOUTH);
+		
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setBounds(600, 200, 800, 600);
+		setVisible(true);
+	}
+}
