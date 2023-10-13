7ea322e01b363f39debc5c0e8f0c97b9be0df046
Borja Rabade <borja.rabade@opendeusto.es>
2023-10-13 10:42:27 +0200

    Inicializar en VentanaInicioSesion

--- ProyectoProgramacion3/src/gestionFacturas/Main.java
+++ ProyectoProgramacion3/src/gestionFacturas/Main.java
@@ -3,6 +3,5 @@ package gestionFacturas;
 public class Main {
 	public static void main(String[] args) {
 		VentanaInicioSesion vis = new VentanaInicioSesion();
-		//VentanaPrincipal vp = new VentanaPrincipal();
 	}
 }
