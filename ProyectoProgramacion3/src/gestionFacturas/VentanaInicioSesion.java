0335fd0d5c82419ee46cb64bb57ce90ce2fb5452
Borja Rabade <borja.rabade@opendeusto.es>
2023-10-13 10:41:58 +0200

    Creada la ventana de inicio de sesión y vincularla a un fichero para el inicio de sesión de cada usuario ya registrado en ese fichero que funciona como base de datos simple

--- ProyectoProgramacion3/src/gestionFacturas/VentanaInicioSesion.java
+++ ProyectoProgramacion3/src/gestionFacturas/VentanaInicioSesion.java
@@ -6,28 +6,81 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class VentanaInicioSesion extends JFrame{
-	private JButton botonAceptar, botonRegistro, botonCerrar;
+	private JButton botonInicioSesion, botonRegistro, botonCerrar;
 	private JTextField txtUsuario;
 	private JPasswordField txtContrasenya;
 	private JLabel labelUsuario, labelContrasenya;
 	private JPanel pDatos, pBotones;
+	private JFrame vActual;
 	
 	public VentanaInicioSesion() {
-		
+		vActual = this;
 		/*CREACIÓN DE PANELES Y COMPONENTES*/
 		pDatos = new JPanel(new GridLayout(2,2));
 		pBotones = new JPanel(new GridLayout(1,3));
 		getContentPane().add(pDatos, BorderLayout.NORTH);
 		getContentPane().add(pBotones, BorderLayout.SOUTH);
 		
-		setDefaultCloseOperation(EXIT_ON_CLOSE); //cambiarlo más tarde a DO_NOTHING_ON_CLOSE
-		setBounds(300, 200, 600, 400);
+		labelUsuario = new JLabel("USUARIO: ");
+		labelContrasenya = new JLabel("CONTRASEÑA: ");
+		txtUsuario = new JTextField();
+		txtContrasenya = new JPasswordField();
+		pDatos.add(labelUsuario);
+		pDatos.add(labelContrasenya);
+		pDatos.add(txtUsuario);
+		pDatos.add(txtContrasenya);
+		
+		botonInicioSesion = new JButton("INICIAR SESIÓN");
+		botonRegistro = new JButton("REGISTRARSE");
+		botonCerrar = new JButton("SALIR");
+		pBotones.add(botonInicioSesion);
+		pBotones.add(botonRegistro);
+		pBotones.add(botonCerrar);
+		
+		/*CARGAMOS LAS COLECCIONES CON LOS DATOS INICIALES*/
+		BaseDatos.cargarFicheroUsuariosEnLista("./datos/BaseDeDatos.csv");
+		
+		/*EVENTOS*/
+		botonCerrar.addActionListener((e)->{
+			BaseDatos.guardarListaUsuariosEnFichero("./datos/BaseDeDatos.csv");
+			System.exit(0);
+		});
+		
+		botonInicioSesion.addActionListener((e)->{
+			String usuario = txtUsuario.getText();
+			String contrasenya = txtContrasenya.getText();
+			Usuario u = BaseDatos.buscarUsuario(usuario);
+			if(u == null) {
+				JOptionPane.showMessageDialog(null, "El usuario no está registrado","ERROR EN EL INICIO DE SESIÓN",JOptionPane.ERROR_MESSAGE);
+			}else if(u.getContrasenya().equals(contrasenya)) {
+				JOptionPane.showMessageDialog(null, "Bienvenido!!","INICIO DE SESIÓN",JOptionPane.INFORMATION_MESSAGE);
+				new VentanaPrincipal();
+				vActual.dispose();
+			}else {
+				JOptionPane.showMessageDialog(null, "La contraseña no es correcta","ERROR EN EL INICIO DE SESIÓN",JOptionPane.WARNING_MESSAGE);
+			}
+			limpiarCampos();
+		});
+		
+		botonRegistro.addActionListener((e)->{
+			new VentanaRegistro();
+			vActual.dispose();
+		});
+		
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		setBounds(300, 200, 600, 100);
 		setTitle("VentanaInicioSesion");
 		setVisible(true);
+	}
+	
+	private void limpiarCampos() {
+		txtUsuario.setText("");
+		txtContrasenya.setText("");
 	}
 }
