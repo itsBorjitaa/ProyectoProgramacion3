056091ba257ecd2bab805d906733fc451acd2676
Borja Rabade <borja.rabade@opendeusto.es>
2023-10-24 17:13:19 +0200

    Creada la JList en el panel de la izquierda

--- ProyectoProgramacion3/src/gestionFacturas/VentanaCategorias.java
+++ ProyectoProgramacion3/src/gestionFacturas/VentanaCategorias.java
@@ -1,21 +1,24 @@
 package gestionFacturas;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 public class VentanaCategorias extends JFrame {
 	private JButton botonVolver, botonModificar, botonBorrar, botonAnyadir;
-	private JScrollPane scrollClases;
+	private JScrollPane scrollListaCategorias;
 	private JPanel panelDerechaBotones, panelIzquierda;
-	private ArrayList<ClasesTickets> listaClases;
+	private DefaultListModel<Categoria> modeloListaCategorias;
+	private JList<Categoria> listaCategorias;
 	private JFrame vActual;
 	
 	public VentanaCategorias() {
@@ -23,17 +26,39 @@ public class VentanaCategorias extends JFrame {
 		
 		/*CREACIÓN DE PANELES*/
 		panelDerechaBotones = new JPanel(new GridLayout(4,1));
-		panelIzquierda = new JPanel();
+		panelIzquierda = new JPanel(new BorderLayout());
 		
-		/*CREACION COMPONENTES*/
-		scrollClases = new JScrollPane();
+		/*CREACION COMPONENTES DE BOTONES*/
 		botonVolver = new JButton("Volver");
 		botonModificar = new JButton("Modificar");
 		botonBorrar = new JButton("Borrar");
 		botonAnyadir = new JButton("Añadir");
 		
-		/*AÑADIR COMPONENTES A PANELES*/
-		panelIzquierda.add(scrollClases);
+		/*CREACIÓN DE LA JLIST DE CATEGORIAS*/
+		modeloListaCategorias = new DefaultListModel<>();
+		listaCategorias = new JList<>(modeloListaCategorias);
+		scrollListaCategorias = new JScrollPane(listaCategorias);
+		scrollListaCategorias.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scrollListaCategorias.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		cargarCategorias();
+		panelIzquierda.add(scrollListaCategorias);
+		
+		/*RENDERER DE LA JLIST*/
+		
+		listaCategorias.setCellRenderer(new DefaultListCellRenderer() {
+			public Component getListCellRendererComponent(JList<?> list,
+                    Object value,
+                    int index,
+                    boolean isSelected,
+                    boolean cellHasFocus) {
+				
+				Component c = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+				return c;
+				
+			}
+		});
+		
+		/*AÑADIR COMPONENTES A PANEL BOTONES*/
 		panelDerechaBotones.add(botonAnyadir);
 		panelDerechaBotones.add(botonBorrar);
 		panelDerechaBotones.add(botonModificar);
@@ -89,5 +114,12 @@ public class VentanaCategorias extends JFrame {
 		setBounds(450, 300, 600, 400);
 		setTitle("Ventana Categorias");
 		setVisible(true);
+	}
+	
+	private void cargarCategorias() {
+		//Recorremos el conjunto de categorias de la clase Base Datos para cargarlos en la JList de esta ventana
+		for(Categoria c: BaseDatos.getCategorias()) { //Por cada Categoria que haya en la lista de categorias
+			modeloListaCategorias.addElement(c); //Lo añadimos al modelo de la JList
+		}
 	}
 }
