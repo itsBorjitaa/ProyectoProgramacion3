ef5ccb3e5b2c71d0dde6e3900bb25a135621da82
Eneko Saez <eneko.saez@opendeusto.es>
2023-11-14 14:43:51 +0100

    Creada ventana cargar

---
+++ ProyectoProgramacion3/src/ventanas/VentanaCargar.java
@@ -0,0 +1,48 @@
+package ventanas;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+
+import java.util.logging.Logger;
+
+public class VentanaCargar extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JProgressBar barraProgreso;
+	private JLabel lblProgreso;
+	private JFrame vActual;
+	private Logger logger = Logger.getLogger(VentanaInicioSesion.class.getName());
+	
+	public VentanaCargar () {
+		vActual = this;
+		/*CREACION DE PANELES*/
+		JPanel pArriba = new JPanel();
+		JPanel pAbajo = new JPanel();
+		getContentPane().add(pArriba, BorderLayout.NORTH);
+		getContentPane().add(pAbajo, BorderLayout.SOUTH);
+		logger.info("Paneles creados");
+		
+		/*CREACION DE COMPONENETES*/
+		barraProgreso = new JProgressBar();
+		lblProgreso = new JLabel("PRUEBAS");
+		pAbajo.add(lblProgreso);
+		pArriba.add(barraProgreso);
+		logger.info("Añaidos barra de progreso y label al panel principal");
+		
+		/*ESPECIFICACION VENTANA*/
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		setBounds(450, 300, 600, 100);
+		setTitle("VentanaInicioSesion");
+		setVisible(true);
+	}
+	
+	
+}
